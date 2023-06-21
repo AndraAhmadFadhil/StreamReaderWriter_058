6ec4d4a63a1424a139f0c8319689f9655ab6d8f7
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2023-06-21 10:25:58 +0700

    menambahkan while untuk membuat unlimited loop untuk menulis pada fungsi utama

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -10,4 +10,10 @@ int main() {
 	ofstream outfile;
 	//menunjuk ke sebuah nama file.
 	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	while (true) {
+
+	}
 }
